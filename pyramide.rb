34afe87341166cddb265872beba394aa60c63057
JulienTHP <45801226+JulienTHP@users.noreply.github.com>
2018-12-28 18:21:32 +0100

    Update pyramide.rb

--- pyramide.rb
+++ pyramide.rb
@@ -5,8 +5,6 @@ number_of_floor = gets.chomp.to_i
 				
 number_of_bricks = 0
 
-number_of_space = 0
-
 while number_of_bricks != number_of_floor
 	i = number_of_bricks + 1
 	i_b = number_of_floor - number_of_bricks
